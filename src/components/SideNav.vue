183d711e4e94cbd126d317a79440a4fd5de106df
wangweijiu <Weijiu.Wang@daxuezhang.com>
2018-08-17 16:37:32 +0800

    修改侧边栏

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -2,7 +2,20 @@
   <el-menu class="" router>
     <el-menu-item index="">首页</el-menu-item>
     <el-menu-item index="">系统管理</el-menu-item>
-    <el-menu-item index="">用户管理</el-menu-item>
+    <el-submenu index="1">
+      <template slot="title">用户管理</template>
+      <el-menu-item-group>
+        <template slot="title">人员管理</template>
+        <el-menu-item index="">用户添加编辑页</el-menu-item>
+        <el-menu-item index="">删除提示窗口</el-menu-item>
+        <el-menu-item index="">禁用编辑窗口</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group>
+        <template slot="title">实名认证列表</template>
+        <el-menu-item index="">实名认证信息详情页</el-menu-item>
+        <el-menu-item index="">撤销提示窗口</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
     <el-menu-item index="">商品管理</el-menu-item>
     <el-menu-item index="">文章管理</el-menu-item>
     <el-menu-item index="">问答管理</el-menu-item>
@@ -13,8 +26,25 @@
     <el-menu-item index="">媒资管理</el-menu-item>
     <el-menu-item index="">数据中心</el-menu-item>
 
-    <el-menu-item index="">机构院校管理</el-menu-item>
-    <el-menu-item index="">关注管理</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">机构院校管理</template>
+      <el-menu-item-group>
+        <template slot="title">机构院校列表</template>
+        <el-menu-item index="">机构院校新增/编辑页</el-menu-item>
+        <el-menu-item index="">禁用编辑窗口</el-menu-item>
+        <el-menu-item index="">删除编辑窗口</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group>
+        <template slot="title">机构院校审核列表</template>
+        <el-menu-item index="">机构院校审核详情页</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+    <el-submenu index="3">
+      <template slot="title">关注管理</template>
+      <el-menu-item-group>
+        <el-menu-item index="">关注管理列表</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
     <el-menu-item index="">私信管理</el-menu-item>
     <el-menu-item index="">课程管理</el-menu-item>
     <el-menu-item index="">出版物管理</el-menu-item>
