e4494166aeb6132c3439ba8539513e90db2ef402
huangrongfa <rongfa.huang@daxuezhang.com>
2018-08-18 17:05:45 +0800

    开发课程管理相关page

---
+++ src/views/courseCommentDetail.vue
@@ -0,0 +1,144 @@
+<template>
+  <section class="coursecomment-detail">
+      <el-row :gutter="20">
+          <el-form :inline="true" class="demo-form-inline" label-width="80px" size="mini">
+              <el-col :span="6">
+                  <el-form-item label="课程ID">
+                      <el-input type="text" :disabled="true"></el-input>
+                  </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                  <el-form-item label="课程标题">
+                      <el-input type="text" :disabled="true"></el-input>
+                  </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                  <el-form-item label="讲师名称">
+                      <el-input type="text" :disabled="true"></el-input>
+                  </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                  <el-form-item label="好评度">
+                      <el-input type="text" :disabled="true"></el-input>
+                  </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                  <div class="comment-total">
+                      <span>评论总数：<span class="comment-num">1000个</span></span>
+                  </div>
+              </el-col>
+          </el-form>
+        </el-row>
+        <!--  -->
+        <hr>
+      <el-row :gutter="20">
+              <el-form :inline="true" class="demo-form-inline" label-width="80px" size="mini">
+                  <el-col :span="6">
+                      <el-form-item label="评论内容">
+                          <el-input type="text"></el-input>
+                      </el-form-item>
+                  </el-col>
+                  <el-col :span="6">
+                      <el-form-item label="评论人">
+                          <el-input type="text"></el-input>
+                      </el-form-item>
+                  </el-col>
+                  <el-col :span="6">
+                      <el-form-item label="评论ID">
+                          <el-input type="text"></el-input>
+                      </el-form-item>
+                  </el-col>
+                  <el-col :span="6">
+                      <el-form-item label="评论时间">
+                          <el-input type="text"></el-input>
+                      </el-form-item>
+                  </el-col>
+                  <el-col :span="6">
+                      <el-form-item label="回复内容">
+                          <el-input type="text"></el-input>
+                      </el-form-item>
+                  </el-col>
+                  <el-col :span="6">
+                      <el-form-item label="回复时间">
+                          <el-input type="text"></el-input>
+                      </el-form-item>
+                  </el-col>
+                  <el-col :span="6">
+                      <el-form-item label="评论类型">
+                          <el-select placeholder="全部">
+                              <el-option label="全部"  value="shanghai"></el-option>
+                              <el-option label="好评" value="beijing"></el-option>
+                              <el-option label="中评" value="beijing"></el-option>
+                              <el-option label="差评" value="beijing"></el-option>
+                            </el-select>
+                      </el-form-item>
+                  </el-col>
+                  <el-col :span="3">
+                      <el-button size="small" type="primary">搜索</el-button>
+                  </el-col>
+              </el-form>
+            </el-row>
+            <!-- 表格 -->
+            <el-table :data="tableData3">
+                <el-table-column type="selection" width="55"></el-table-column>
+                <el-table-column prop="date" label="评论ID" width="120"></el-table-column>
+                <el-table-column prop="name" label="评论内容" width="120"></el-table-column>
+                <el-table-column prop="name" label="评论人" width="120"></el-table-column>
+                <el-table-column prop="name" label="评论时间" width="120"></el-table-column>
+                <el-table-column prop="name" label="评论渠道" width="120"></el-table-column>
+                <el-table-column prop="name" label="回复内容" width="120"></el-table-column>
+                <el-table-column prop="name" label="回复时间" width="120"></el-table-column>
+                <el-table-column prop="name" label="回复渠道" width="120"></el-table-column>
+                <el-table-column prop="name" label="评论类型" width="120"></el-table-column>
+                <el-table-column prop="address" label="操作" show-overflow-tooltip></el-table-column>
+            </el-table>
+            <div style="height:30px"></div>
+            <!-- 分页 -->
+            <el-row :gutter="20">
+                <el-col :span="9">
+                    <el-pagination layout="prev, pager, next, jumper" :total="100"></el-pagination>
+                </el-col>
+                <el-col :span="9">
+                    <el-button size="small" type="primary">确定</el-button>
+                </el-col>
+                <el-col :span="5">
+                    <el-button size="small" type="primary">批量删除</el-button>
+                </el-col>
+            </el-row>
+  </section>
+</template>
+<script>
+    export default {
+      name:'courseList',
+      data () {
+          return {
+              tableData3:[{
+                  date: '20160502',
+                  name: '张三',
+                  address: '删除'
+              },{
+                  date: '20160502',
+                  name: '李四',
+                  address: '删除'
+              },{
+                  date: '20160502',
+                  name: '王五',
+                  address: '删除'
+              }]
+          }
+      }
+    }
+  </script>
+<style>
+  .coursecomment-detail{
+    max-width: 1280px;
+    margin: auto;
+  }
+  .coursecomment-detail .comment-total{
+    margin-left:24px;
+    
+  }
+  .comment-num{
+    font-size: 22px;
+  }
+</style>
---
+++ src/views/courseDetail.vue
@@ -0,0 +1,309 @@
+<template>
+  <section class="course-detail">
+    <el-form ref="form" :model="form" label-width="80px" size="mini">
+        <el-row :gutter="20">
+            <el-col :span="5">
+                <el-form-item label="课程ID">
+                    <el-input v-model="form.name" :disabled="true"></el-input>
+                  </el-form-item>
+            </el-col>
+            <el-col :span="5">
+                <el-form-item label="课程标题">
+                    <el-input v-model="form.name"></el-input>
+                  </el-form-item>
+            </el-col>
+            <el-col :span="5">
+                <el-form-item label="讲师名称">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <!--  -->
+        <el-row :gutter="20">
+            <el-col :span="5">
+                <el-form-item label="课程分类">
+                    <el-select placeholder="全部">
+                        <el-option label="全部"  value="shanghai"></el-option>
+                        <el-option label="托福" value="beijing"></el-option>
+                        <el-option label="GRE" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-col>
+            <el-col :span="5">
+                <el-form-item label="课程标签">
+                    <el-select placeholder="全部">
+                        <el-option label="全部"  value="shanghai"></el-option>
+                        <el-option label="语法" value="beijing"></el-option>
+                        <el-option label="词汇" value="beijing"></el-option>
+                    </el-select>
+                  </el-form-item>
+            </el-col>
+            <el-col :span="5">
+                <el-form-item label="课程特色">
+                    <el-col :span="8">
+                        <el-input type="text"></el-input>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-input type="text"></el-input>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-input type="text"></el-input>
+                    </el-col>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <!--  -->
+        <el-row :gutter="20">
+            <el-col :span="5">
+                <el-form-item label="上课模式">
+                    <el-col :span="12">
+                        <el-select placeholder="全部">
+                            <el-option label="全部"  value="shanghai"></el-option>
+                            <el-option label="直播" value="beijing"></el-option>
+                            <el-option label="录播" value="beijing"></el-option>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-select placeholder="全部">
+                            <el-option label="全部"  value="shanghai"></el-option>
+                            <el-option label="直播" value="beijing"></el-option>
+                            <el-option label="录播" value="beijing"></el-option>
+                        </el-select>
+                    </el-col>
+                </el-form-item>
+            </el-col>
+            <el-col :span="5">
+              <el-form-item label="课程价格">
+                  <el-col :span="12">
+                      <el-input v-model="form.name" :disabled=true placeholder="人民币"></el-input>
+                  </el-col>
+                  <el-col :span="12">
+                      <el-input v-model="form.name" :disabled=true placeholder="美元"></el-input>
+                  </el-col>
+              </el-form-item>
+            </el-col>
+            <el-col :span="5">
+                <el-form-item label="限售人数">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <!--  -->
+        <el-form-item label="课程封面">
+          <img src="https://imgsa.baidu.com/news/q%3D100/sign=df2ed05edc09b3deedbfe068fcbf6cd3/1ad5ad6eddc451da08ab7bddbbfd5266d01632a2.jpg" alt="" srcset="" height=200 width=300>
+        </el-form-item>
+        <el-form-item label="课程简介">
+            <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item label="上传课件">
+            <div class="upload-course">
+                <el-input type="text" width="300"></el-input>
+                <el-upload><el-button size="small" type="primary">点击上传</el-button></el-upload>
+            </div>
+        </el-form-item>
+        <el-row :gutter="20">
+          <el-col :span="5">
+              <el-form-item label="上课时间">
+                  <el-input type="text"></el-input>
+              </el-form-item>
+          </el-col>
+          <el-col :span="5">
+              <el-input type="text" size="mini"></el-input>
+          </el-col>
+          <el-col :span="5">
+              <el-input type="text" size="mini"></el-input>
+          </el-col>
+        </el-row>
+        <!--  -->
+        <el-form-item label="课节">
+            <el-row :gutter="20">
+                <el-col :span="5">
+                    <el-form-item label="第一节">
+                      <el-input type="text"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="5">
+                    <el-form-item>
+                        <el-input type="text"></el-input>
+                        <div class="upload-btn">
+                          <el-upload><el-button size="small" type="primary">点击上传</el-button></el-upload>
+                        </div>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+            <el-row :gutter="20">
+                <el-col :span="5">
+                    <el-form-item label="第二节">
+                      <el-input type="text"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="5">
+                    <el-form-item>
+                        <el-input type="text"></el-input>
+                        <div class="upload-btn">
+                            <el-upload>
+                                <el-button size="small" type="primary">点击上传</el-button>
+                              </el-upload>
+                        </div>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+        </el-form-item>
+        <el-row :gutter="20">
+            <el-col :span="7">
+              <div class="grid-content bg-purple course-plane" style="position:relative;">
+                <span class="course-time">下次课程更新时间</span>
+                <el-form-item label="">
+                    <el-input type="text"></el-input>
+                </el-form-item>
+            </div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple">
+              <el-form-item label="课程状态">
+                <el-select>
+                    <el-option label="已完结"  value="shanghai"></el-option>
+                    <el-option label="未完结" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+            </div></el-col>
+        </el-row>
+        <el-form-item label="所属频道">
+            <el-select placeholder="所属频道">
+                <el-option label="全部"  value="全部"></el-option>
+                <el-option label="留学" value="留学"></el-option>
+                <el-option label="语培" value="语培"></el-option>
+            </el-select>
+        </el-form-item>
+        <!-- 按钮集合 -->
+        <div class="center-box">
+            <el-row>
+                <el-button type="primary" size="medium">创建</el-button>
+                <el-button type="primary" size="medium">冻结</el-button>
+                <el-button type="primary" size="medium">删除</el-button>
+                <el-button type="primary" size="medium">取消</el-button>
+            </el-row>
+        </div>
+        <!-- 编辑状态属性 -->
+        <div class="edite-active">
+            <el-row :gutter="20">
+                <el-col :span="5">
+                    <el-form-item label="销售状态">
+                        <el-input type="text" size="mini" placeholder="已上架" :disabled="true"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="5">
+                    <el-form-item label="直播状态">
+                        <el-input type="text" size="mini" placeholder="直播中" :disabled="true"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="5">
+                    <el-form-item label="直播时长">
+                        <el-input type="text" size="mini" placeholder="1小时" :disabled="true"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="5">
+                    <el-form-item label="审核状态">
+                        <el-input type="text" size="mini" :disabled="true" placeholder="待审核"></el-input>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+        </div>
+        <!--  -->
+        <el-form-item label="课程统计">
+            <el-table border style="width: 100%" size="mini">
+            <el-table-column
+              prop="date"
+              label="浏览数"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="转发数"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="分享数">
+            </el-table-column>
+            <el-table-column
+            prop="address"
+            label="收藏数">
+          </el-table-column>
+          <el-table-column
+          prop="address"
+          label="评论数">
+        </el-table-column>
+        <el-table-column
+        prop="address"
+        label="点赞数">
+      </el-table-column>
+      <el-table-column
+      prop="address"
+      label="学生数">
+    </el-table-column>
+    <el-table-column
+    prop="address"
+    label="好评度">
+  </el-table-column>
+          </el-table>
+        </el-form-item>
+      </el-form>
+    </section>
+</template>
+<script>
+    export default {
+      data() {
+        return {
+          form: {
+            name: '',
+            region: '',
+            date1: '',
+            date2: '',
+            delivery: false,
+            type: [],
+            resource: '',
+            desc: ''
+          }
+        }
+      },
+      methods: {
+        onSubmit() {
+          console.log('submit!');
+        }
+      }
+    }
+  </script>
+  <style>
+  .center-box{
+    display: flex;
+    justify-content: center;
+  }
+  .edite-active{
+    border-top:solid 1px #ccc;
+    margin-top: 30px;
+    padding-top: 15px;
+  }
+  .upload-btn{
+    position:absolute;
+    right: -90px;
+    top: -4px;
+  }
+  .course-time{
+    position:absolute;
+    left: 12px;
+  }
+  .course-detail .course-plane .el-form-item__content{
+    margin-left:135px !important;
+  }
+  .upload-course .el-input__inner{
+    width:80% !important;
+  }
+  .upload-course{
+    position:relative;
+    max-width: 480px;
+  }
+  .upload-course .el-upload{
+    position:absolute;
+    top: 0;
+    right: 0;
+  }
+  </style>
